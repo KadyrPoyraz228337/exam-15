d0d7890dcb705ab43fced0706b980e3a7593461e
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-18 10:55:42 +0600

    Добавил updateRating.js

---
+++ backend/service/updateRating.js
@@ -0,0 +1,20 @@
+const Place = require('../models/Place');
+const Review = require('../models/Review');
+
+const roundUp = number =>  +parseFloat(number).toFixed(1);
+
+module.exports = async function (id) {
+    const reviews = await Review.find({place: id})
+    const length = reviews.length;
+
+    const result = reviews.reduce((a, c) => {
+        return {
+            qualityOfFood: roundUp(a.qualityOfFood + c.qualityOfFood/length),
+            serviceQuality: roundUp(a.serviceQuality + c.serviceQuality/length),
+            interior: roundUp(a.interior + c.interior/length)
+        }
+    }, {qualityOfFood: 0, serviceQuality: 0, interior: 0})
+    result.rating = roundUp((result.qualityOfFood + result.serviceQuality + result.interior) / 3)
+
+    await Place.update({_id: id}, result)
+}
