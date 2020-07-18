90a5fa0c47d8d96548a44a69f3816cfa7460566a
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-18 09:43:19 +0600

    Добавил модель Place.js

---
+++ backend/models/Place.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const ratingValidator = {
+    validator: function (v) {
+        return v <= 5 && v > -1
+    },
+    message: props => `${props.value} is not a valid rating!`
+}
+
+const RecipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    image: String,
+    description: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    qualityOfFood: {
+        type: Number,
+        validate: ratingValidator,
+        default: 0
+    },
+    serviceQuality: {
+        type: Number,
+        validate: ratingValidator,
+        default: 0
+    },
+    interior: {
+        type: Number,
+        validate: ratingValidator,
+        default: 0
+    }
+})
+
+const modelName = 'Recipe'
+
+module.exports = mongoose.model(modelName, RecipeSchema);
